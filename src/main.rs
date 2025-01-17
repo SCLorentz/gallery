d7b6f5bf9a8787c5d752cddcd357f960bedd8c11
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2025-01-17 16:06:46 +0000

    working decompression now

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
 use std::{
-    io::{self, Read, Write, prelude::*},
-    fs::File,
-    env
+    io::prelude::*,
+    fs::{self, File}
 };
 
 use flate2::Compression;
-use flate2::write::ZlibEncoder;
 use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
 
 fn main()
 {
-    //let _ = compress();
-    let _ = uncompress();
+    match std::env::args().nth(1)
+    {
+        Some(val) if val == "encode".to_string() => encode().unwrap_or_else(|err| eprintln!("{}", err)),
+        _ => decode().unwrap_or_else(|err| eprintln!("{}", err)),
+    }
 }
 
-fn compress() -> Result<(), io::Error>
-{    
-    let mut input = File::open("./file.svg").unwrap();
-    let mut output = File::create("./file.svgz").unwrap();
+fn encode() -> Result<(), std::io::Error>
+{
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
 
-    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    let content = fs::read("./file.svg")?;
+    encoder.write_all(&content)?;
 
-    io::copy(&mut input, &mut encoder)?;
-    let compressed_data = encoder.finish()?;
+    let compressed_bytes = encoder.finish()?;
 
-    output.write_all(&compressed_data)?;
+    let mut output = File::create("./file.svgz")?;
+    output.write_all(&compressed_bytes)?;
 
     Ok(())
 }
 
-fn uncompress() -> Result<(), io::Error>
+fn decode() -> Result<(), std::io::Error>
 {
-    let compressed_data = std::fs::read("./file.svgz")?;
+    let content = fs::read("./file.svgz")?;
 
-    let mut decoder = GzDecoder::new(compressed_data.as_slice());
+    let mut d = GzDecoder::new(content.as_slice());
+
+    let mut buff = Vec::new();
+
+    d.read_to_end(&mut buff)?;
 
     let mut output = File::create("./file.test.svg")?;
-
-    io::copy(&mut decoder, &mut output)?;
+    output.write_all(&buff)?;
 
     Ok(())
 }
